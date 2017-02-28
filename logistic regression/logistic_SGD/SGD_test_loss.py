a2f0cc2cb0218ec5c1d4009d77f96da85c700c88
ouyangyike <yike.ouyang@mail.utoronto.ca>
2017-02-27 20:35:33 -0500

    logistic regression

---
+++ logistic regression/logistic_SGD/SGD_test_loss.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from logistic_SGD import *
+
+
+#learing rate = 1,batch_size = 500, epoch=13, lamda = 0.01
+logging = runLogistic(1,500,13,0.01)
+#print(logging)
+plt.plot(logging[:,2],marker='+',label='learning rate = 1')
+
+#learing rate = 0.1,batch_size = 500, epoch=13, lamda = 0.01
+logging = runLogistic(0.1,500,13,0.01)
+#print(logging)
+plt.plot(logging[:,2],marker='*',label='learning rate = 0.1')
+
+#learing rate = 0.01,batch_size = 500, epoch=13, lamda = 0.01
+logging = runLogistic(0.01,500,13,0.01)
+#print(logging)
+plt.plot(logging[:,2],marker='h',label='learning rate = 0.01')
+
+#learing rate = 0.001,batch_size = 500, epoch=13, lamda = 0.01
+logging = runLogistic(0.001,500,13,0.01)
+#print(logging)
+plt.plot(logging[:,2],marker='d',label='learning rate = 0.001')
+
+
+plt.legend(loc='upper right')
+plt.title('Plot of Test_CrossEntropy vs. Iterations with batch_size=500')
+plt.xlabel('Iterations')
+plt.ylabel('Test_CrossEntropy')
+plt.show()
+
+
